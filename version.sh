35af591566129c09a561dc66ebacda7b9bb9bb70
Geoff Levand <geoff@infradead.org>
2021-05-07 19:46:23 -0700

    Autotools cleanup
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#!/usr/bin/env bash
 #
-# version.sh: create a version string for use by configure.ac
+# version.sh: Create a version string for use by configure.ac
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,42 +18,40 @@
 version=
 datefmt='%Y%m%d'
 
-export GIT_DIR=$(dirname $0)/.git/
+GIT_DIR=$(dirname "${0}")/.git/
+export GIT_DIR
 
-if head=$(git rev-parse --short=8 --verify HEAD 2>/dev/null); then
-
+if head="$(git rev-parse --short=8 --verify HEAD 2>/dev/null)"; then
 	suffix=''
 	# Add a '-dirty' suffix for uncommitted changes.
-	if git diff-index HEAD | read dummy; then
-		suffix=-dirty
+	if git diff-index HEAD | read -r dummy; then
+		suffix='-dirty'
 	fi
 
-	if tag=$(git describe --tags --exact-match 2>/dev/null); then
+	if tag="$(git describe --tags --exact-match 2>/dev/null)"; then
 		# use a tag; remove any 'v' prefix from v<VERSION> tags
-		tag=${tag#v}
-		version=$(printf "%s%s" ${tag} ${suffix})
+		tag="${tag#v}"
+		version="$(printf "%s%s" "${tag}" "${suffix}")"
 	else
 		# Use the git commit revision for the package version, and add
 		# a date prefix for easy comparisons.
-		date=$(git log --pretty=format:"%ct" -1 HEAD)
-		version=$(printf "%($datefmt)T.g%s%s" ${date} ${head} ${suffix})
+		date="$(git log --pretty=format:"%ct" -1 HEAD)"
+		version="$(printf "%($datefmt)T.g%s%s" "${date}" "${head}" "${suffix}")"
 	fi
 else
 	# Check if a specific version is set, eg: by buildroot
-	if [ ! -z "$PACKAGE_VERSION" ];
-	then
+	if [ -n "${PACKAGE_VERSION}" ]; then
 		# Full git hash
-		len=$(echo -n "${PACKAGE_VERSION}" | wc -c)
+		len="$(echo -n "${PACKAGE_VERSION}" | wc -c)"
 		if [[ ${len} == 40 ]]; then
-			version=`echo -n ${PACKAGE_VERSION} | \
-				sed "s/^\([0-9a-f]\{7\}\).*/\1/;"`
+			version="$(echo -n "${PACKAGE_VERSION}" | sed 's/^\([0-9a-f]\{7\}\).*/\1/;')"
 		else
-			version="$PACKAGE_VERSION"
+			version="${PACKAGE_VERSION}"
 		fi
 	else
 		# Default to current date and time.
-		version="$(date +dev.$datefmt)"
+		version="$(date +dev.${datefmt})"
 	fi
 fi
 
-echo $version
+echo "${version}"
